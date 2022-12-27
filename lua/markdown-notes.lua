994f2c1d245d7241ff677e30cdd5914de45556e6
njaczko <nick@jaczko.com>
2022-12-27 18:49:38 -0500

    fix highlight links

--- lua/markdown-notes.lua
+++ lua/markdown-notes.lua
@@ -2,33 +2,28 @@ local M = {}
 
 -- TODO doc string
 function M.markdownNotes()
-  -- vim.api.nvim_set_hl(0, "your-group", { link = "another-group" })
-  -- vim.cmd('echo 42')
   vim.cmd('syntax sync linebreaks=1')
 
   -- TODO dry up regexes with string interpolation?
-
-  -- TODO doesn't seem to be working yet...
   vim.cmd([[syntax match notesDoneItem /^\(\s\+\).*\<DONE\>.*\(\n\1\s.*\)*/]])
   vim.cmd([[syntax match notesDoneMarker /\<DONE\>/ containedin=notesDoneItem]])
-  vim.api.nvim_set_hl(0, "Comment", {link = "notesDoneItem" })
-  vim.api.nvim_set_hl(0, "Question", {link = "notesDoneMarker" })
-
+  vim.api.nvim_set_hl(0, "notesDoneMarker", {link = "Question" })
+  vim.api.nvim_set_hl(0, "notesDoneItem", {link = "Comment" })
 
   vim.cmd([[syntax match notesBlockedItem /^\(\s\+\).*\<BLOCKED\>.*\(\n\1\s.*\)*/]])
   vim.cmd([[syntax match notesBlockedMarker /\<BLOCKED\>/ containedin=notesBlockedItem]])
-  vim.api.nvim_set_hl(0, "Comment", {link = "notesBlockedItem" })
-  vim.api.nvim_set_hl(0, "Directory", {link = "notesBlockedMarker" })
+  vim.api.nvim_set_hl(0, "notesBlockedItem", {link = "Comment" })
+  vim.api.nvim_set_hl(0, "notesBlockedMarker", {link = "Directory" })
 
   vim.cmd([[syntax match notesXXXItem /^\(\s\+\).*\<XXX\>.*\(\n\1\s.*\)*/]])
   vim.cmd([[syntax match notesXXXMarker /\<XXX\>/ containedin=notesXXXItem]])
-  vim.api.nvim_set_hl(0, "Comment", {link = "notesXXXItem" })
-  vim.api.nvim_set_hl(0, "WarningMsg", {link = "notesXXXMarker" })
+  vim.api.nvim_set_hl(0, "notesXXXItem", {link = "Comment" })
+  vim.api.nvim_set_hl(0, "notesXXXMarker", {link = "WarningMsg" })
 
   vim.cmd([[syntax match notesTodo /\<TODO\>/]])
   vim.cmd([[syntax match notesInProgress /\<WIP\>/]])
-  vim.api.nvim_set_hl(0, "WarningMsg", {link = "notesTodo" })
-  vim.api.nvim_set_hl(0, "Directory", {link = "notesInProgress" })
+  vim.api.nvim_set_hl(0, "notesTodo", {link = "WarningMsg" })
+  vim.api.nvim_set_hl(0, "notesInProgress", {link = "Directory" })
 end
 
 return M
