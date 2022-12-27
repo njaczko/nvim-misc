450de509aaaad546974e807d88296bf2eeb71c81
njaczko <nick@jaczko.com>
2022-12-27 18:26:52 -0500

    Fix escaping errors

--- lua/markdown-notes.lua
+++ lua/markdown-notes.lua
@@ -7,24 +7,26 @@ function M.markdownNotes()
   vim.cmd('syntax sync linebreaks=1')
 
   -- TODO dry up regexes with string interpolation?
-  vim.cmd('syntax match notesDoneItem /^\(\s\+\).*\<DONE\>.*\(\n\1\s.*\)*/')
-  vim.cmd('syntax match notesDoneMarker /\<DONE\>/ containedin=notesDoneItem')
+
+  -- TODO doesn't seem to be working yet...
+  vim.cmd([[syntax match notesDoneItem /^\(\s\+\).*\<DONE\>.*\(\n\1\s.*\)*/]])
+  vim.cmd([[syntax match notesDoneMarker /\<DONE\>/ containedin=notesDoneItem]])
   vim.api.nvim_set_hl(0, "Comment", {link = "notesDoneItem" })
   vim.api.nvim_set_hl(0, "Question", {link = "notesDoneMarker" })
 
 
-  vim.cmd('syntax match notesBlockedItem /^\(\s\+\).*\<BLOCKED\>.*\(\n\1\s.*\)*/')
-  vim.cmd('syntax match notesBlockedMarker /\<BLOCKED\>/ containedin=notesBlockedItem')
+  vim.cmd([[syntax match notesBlockedItem /^\(\s\+\).*\<BLOCKED\>.*\(\n\1\s.*\)*/]])
+  vim.cmd([[syntax match notesBlockedMarker /\<BLOCKED\>/ containedin=notesBlockedItem]])
   vim.api.nvim_set_hl(0, "Comment", {link = "notesBlockedItem" })
   vim.api.nvim_set_hl(0, "Directory", {link = "notesBlockedMarker" })
 
-  vim.cmd('syntax match notesXXXItem /^\(\s\+\).*\<XXX\>.*\(\n\1\s.*\)*/')
-  vim.cmd('syntax match notesXXXMarker /\<XXX\>/ containedin=notesXXXItem')
+  vim.cmd([[syntax match notesXXXItem /^\(\s\+\).*\<XXX\>.*\(\n\1\s.*\)*/]])
+  vim.cmd([[syntax match notesXXXMarker /\<XXX\>/ containedin=notesXXXItem]])
   vim.api.nvim_set_hl(0, "Comment", {link = "notesXXXItem" })
   vim.api.nvim_set_hl(0, "WarningMsg", {link = "notesXXXMarker" })
 
-  vim.cmd('syntax match notesTodo /\<TODO\>/')
-  vim.cmd('syntax match notesInProgress /\<WIP\>/')
+  vim.cmd([[syntax match notesTodo /\<TODO\>/]])
+  vim.cmd([[syntax match notesInProgress /\<WIP\>/]])
   vim.api.nvim_set_hl(0, "WarningMsg", {link = "notesTodo" })
   vim.api.nvim_set_hl(0, "Directory", {link = "notesInProgress" })
 end
