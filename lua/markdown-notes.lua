6f139a0fbf12e84d5da4c18866945a4ec29f5d0a
njaczko <nick@jaczko.com>
2022-12-28 17:09:32 -0500

    add syntax highlighting for markdown notes

---
+++ lua/markdown-notes.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+-- markdownNotes provides highlighted keywords for bulleted note taking, as
+-- inspired by the vim-notes plugin: https://github.com/xolox/vim-notes
+function M.markdownNotes()
+  vim.api.nvim_set_option_value('filetype', 'markdown')
+
+  -- NOTE: if we have more issues with the redrawing in the multi-line highlight
+  -- groups we may have to increase this number. see `:h syn-sync-linebreaks`
+  vim.cmd('syntax sync linebreaks=1')
+
+  vim.cmd([[syntax match notesDoneItem ]] .. itemRegex('DONE'))
+  vim.cmd([[syntax match notesDoneMarker /\<DONE\>/ containedin=notesDoneItem]])
+  vim.api.nvim_set_hl(0, "notesDoneMarker", {link = "Question" })
+  vim.api.nvim_set_hl(0, "notesDoneItem", {link = "Comment" })
+
+  vim.cmd([[syntax match notesBlockedItem ]] .. itemRegex('BLOCKED'))
+  vim.cmd([[syntax match notesBlockedMarker /\<BLOCKED\>/ containedin=notesBlockedItem]])
+  vim.api.nvim_set_hl(0, "notesBlockedItem", {link = "Comment" })
+  vim.api.nvim_set_hl(0, "notesBlockedMarker", {link = "Directory" })
+
+  vim.cmd([[syntax match notesXXXItem ]] .. itemRegex('XXX'))
+  vim.cmd([[syntax match notesXXXMarker /\<XXX\>/ containedin=notesXXXItem]])
+  vim.api.nvim_set_hl(0, "notesXXXItem", {link = "Comment" })
+  vim.api.nvim_set_hl(0, "notesXXXMarker", {link = "WarningMsg" })
+
+  vim.cmd([[syntax match notesTodo /\<TODO\>/]])
+  vim.api.nvim_set_hl(0, "notesTodo", {link = "WarningMsg" })
+
+  vim.cmd([[syntax match notesInProgress /\<WIP\>/]])
+  vim.api.nvim_set_hl(0, "notesInProgress", {link = "Directory" })
+end
+
+function itemRegex(word)
+  return [[/^\(\s\+\).*\<]] .. word .. [[\>.*\(\n\1\s.*\)*/]]
+end
+return M
--- plugin/nvim-misc.vim
+++ plugin/nvim-misc.vim
@@ -4,4 +4,8 @@ endif
 let g:loaded_nvim_misc = 1
 
 command! OpenGithub lua require("open-github").openGithub()
+
 command! FmtCert lua require("fmt-cert").formatX509Cert()
+
+command! MarkdownNotes lua require("markdown-notes").markdownNotes()
+autocmd FileType markdownnotes lua require("markdown-notes").markdownNotes()
