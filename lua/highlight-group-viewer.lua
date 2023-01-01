e93d758b9d329eff1c448e4fa53af9b944ea23d9
njaczko <nick@jaczko.com>
2023-01-01 15:29:01 -0500

    wip - highlight group viewer

---
+++ lua/highlight-group-viewer.lua
@@ -0,0 +1,31 @@
+M = {}
+
+local builtin_groups = {
+  "ColorColumn", "Conceal", "CurSearch", "Cursor", "CursorColumn", "CursorIM",
+  "CursorLine", "CursorLineFold", "CursorLineNr", "CursorLineSign", "DiffAdd",
+  "DiffChange", "DiffDelete", "DiffText", "Directory", "EndOfBuffer", "ErrorMsg",
+  "FoldColumn", "Folded", "IncSearch", "LineNr", "LineNrAbove", "LineNrBelow",
+  "MatchParen", "ModeMsg", "MoreMsg", "MsgArea", "MsgSeparator", "NonText",
+  "Normal", "NormalFloat", "NormalNC", "Pmenu", "PmenuSbar", "PmenuSel",
+  "PmenuThumb", "Question", "QuickFixLine", "Search", "SignColumn", "SpecialKey",
+  "SpellBad", "SpellCap", "SpellLocal", "SpellRare", "StatusLine",
+  "StatusLineNC", "Substitute", "TabLine", "TabLineFill", "TabLineSel",
+  "TermCursor", "TermCursorNC", "Title", "Visual", "VisualNOS", "WarningMsg",
+  "Whitespace", "WildMenu", "WinBar", "WinBarNC", "WinSeparator", "lCursor",
+}
+
+function M.highlightGroupViewer()
+  -- TODO open fresh buffer and write to rather than using the current buffer
+
+  for k,v in pairs(builtin_groups) do
+    vim.cmd(
+      [[syntax match ]] .. v .. [[ /\<]] .. v .. [[\>/]]
+    )
+  end
+
+  vim.api.nvim_buf_set_lines(0, 0, 0, true, builtin_groups)
+  -- wrap lines
+  vim.api.nvim_feedkeys('gggqG' , 'n', true)
+end
+
+return M
--- plugin/nvim-misc.vim
+++ plugin/nvim-misc.vim
@@ -9,3 +9,5 @@ command! FmtCert lua require("fmt-cert").formatX509Cert()
 
 command! MarkdownNotes lua require("markdown-notes").markdownNotes()
 autocmd FileType markdown.markdownnotes lua require("markdown-notes").markdownNotes()
+
+command! HighlightGroupViewer lua require("highlight-group-viewer").highlightGroupViewer()
