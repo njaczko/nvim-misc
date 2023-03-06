27391dac9da8aee05d56beca68665bc684cffaaf
njaczko <nick@jaczko.com>
2023-03-06 14:30:14 -0800

    [markdown-notes] standardize quotes and ignore missing matches in replaceKeyword

--- lua/markdown-notes.lua
+++ lua/markdown-notes.lua
@@ -4,22 +4,22 @@ local M = {}
 -- inspired by the vim-notes plugin: https://github.com/xolox/vim-notes
 function M.markdownNotes()
   -- needed for redrawing the multi-line highlight groups correctly. see `:h syn-sync-linebreaks`
-  vim.cmd('syntax sync linebreaks=1')
+  vim.cmd("syntax sync linebreaks=1")
 
   -- case sensitive matching
-  vim.cmd('syntax case match')
+  vim.cmd("syntax case match")
 
-  vim.cmd([[syntax match notesDoneItem ]] .. itemRegex('DONE'))
+  vim.cmd([[syntax match notesDoneItem ]] .. itemRegex("DONE"))
   vim.cmd([[syntax match notesDoneMarker /\<DONE\>/ containedin=notesDoneItem]])
   vim.api.nvim_set_hl(0, "notesDoneMarker", {link = "Question" })
   vim.api.nvim_set_hl(0, "notesDoneItem", {link = "Comment" })
 
-  vim.cmd([[syntax match notesBlockedItem ]] .. itemRegex('BLOCKED'))
+  vim.cmd([[syntax match notesBlockedItem ]] .. itemRegex("BLOCKED"))
   vim.cmd([[syntax match notesBlockedMarker /\<BLOCKED\>/ containedin=notesBlockedItem]])
   vim.api.nvim_set_hl(0, "notesBlockedItem", {link = "Comment" })
   vim.api.nvim_set_hl(0, "notesBlockedMarker", {link = "Directory" })
 
-  vim.cmd([[syntax match notesXXXItem ]] .. itemRegex('XXX'))
+  vim.cmd([[syntax match notesXXXItem ]] .. itemRegex("XXX"))
   vim.cmd([[syntax match notesXXXMarker /\<XXX\>/ containedin=notesXXXItem]])
   vim.api.nvim_set_hl(0, "notesXXXItem", {link = "Comment" })
   vim.api.nvim_set_hl(0, "notesXXXMarker", {link = "WarningMsg" })
@@ -40,16 +40,16 @@ end
 
 -- replaceKeyword replaces item keyword in the current line
 function replaceKeyword(word)
-  vim.cmd([[s/TODO\|WIP\|DONE\|XXX\|BLOCKED/]] .. word)
-  vim.cmd('noh')
+  vim.cmd([[s/TODO\|WIP\|DONE\|XXX\|BLOCKED/]] .. word .. "/e")
+  vim.cmd("noh")
 end
 
 function defineKeywordReplacementCommands()
-  vim.cmd[[command! Todo lua replaceKeyword('TODO')]]
-  vim.cmd[[command! Wip lua replaceKeyword('WIP')]]
-  vim.cmd[[command! Done lua replaceKeyword('DONE')]]
-  vim.cmd[[command! Xxx lua replaceKeyword('XXX')]]
-  vim.cmd[[command! Blocked lua replaceKeyword('BLOCKED')]]
+  vim.cmd[[command! Todo lua replaceKeyword("TODO")]]
+  vim.cmd[[command! Wip lua replaceKeyword("WIP")]]
+  vim.cmd[[command! Done lua replaceKeyword("DONE")]]
+  vim.cmd[[command! Xxx lua replaceKeyword("XXX")]]
+  vim.cmd[[command! Blocked lua replaceKeyword("BLOCKED")]]
 end
 
 return M
